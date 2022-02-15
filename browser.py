ded10694836920e3787d69c058ff1c26f7bc2786
Ross Wollman <rwoll@users.noreply.github.com>
2022-02-15 01:52:25 +0000

    feat: SSL support

--- browser.py
+++ browser.py
@@ -4,14 +4,18 @@ from dataclasses import dataclass
 import gzip
 from io import BytesIO
 from socket import AF_INET, IPPROTO_TCP, SOCK_STREAM, socket
-from typing import Dict, TextIO, Tuple
+from typing import Dict, Tuple
+import ssl
 
 DEFAULT_HTTP_PORT = 80
 HTTP_PROTOCOL_PREFIX = "http://"
 URL_PATH_SEP = "/"
-CRLF = "\r\n"
-ENCODING = "utf8"
-GZIP_ENCODING = "gzip"
+
+NEWLINE = "\r\n"
+
+ENCODING_UTF8 = "utf8"
+ENCODING_GZIP = "gzip"
+
 HEADER_CONTENT_ENCODING = "content-encoding"
 HEADER_ACCEPT_ENCODING = "accept-encoding"
 
@@ -21,12 +25,15 @@ class URL:
     raw: str
     host: str
     path: str
+    scheme: str
+    port: int
 
     @staticmethod
     def parse(url: str) -> "URL":
         raw = url
-        assert url.startswith(HTTP_PROTOCOL_PREFIX)
-        url = url[len(HTTP_PROTOCOL_PREFIX) :]
+        scheme, url = url.split("://", 1)
+        assert scheme in ["http", "https"]
+        port = 80 if scheme == "http" else 443
 
         if URL_PATH_SEP not in url:
             url += URL_PATH_SEP
@@ -34,36 +41,41 @@ class URL:
         host, path = url.split(URL_PATH_SEP, maxsplit=1)
         path = URL_PATH_SEP + path
 
-        return URL(raw=raw, host=host, path=path)
+        return URL(raw=raw, host=host, path=path, port=port, scheme=scheme)
 
 
 def request(url: str) -> Tuple[Dict[str, str], str]:
     parsed = URL.parse(url)
     s = socket(family=AF_INET, type=SOCK_STREAM, proto=IPPROTO_TCP)
-    s.connect((parsed.host, 80))
+
+    if parsed.scheme == "https":
+        ctx = ssl.create_default_context()
+        s = ctx.wrap_socket(s, server_hostname=parsed.host)
+
+    s.connect((parsed.host, parsed.port))
     s.send(
-        f"GET {parsed.path} HTTP/1.0{CRLF}Host: {parsed.host}{CRLF}{HEADER_ACCEPT_ENCODING}: {GZIP_ENCODING}{CRLF}{CRLF}".encode(
-            ENCODING
+        f"GET {parsed.path} HTTP/1.0{NEWLINE}Host: {parsed.host}{NEWLINE}{HEADER_ACCEPT_ENCODING}: {ENCODING_GZIP}{NEWLINE}{NEWLINE}".encode(
+            ENCODING_UTF8
         )
     )
-    response: BytesIO = s.makefile("rb", encoding=ENCODING, newline=CRLF)
-    statusline = response.readline().decode(ENCODING)
+    response: BytesIO = s.makefile("rb", encoding=ENCODING_UTF8, newline=NEWLINE)
+    statusline = response.readline().decode(ENCODING_UTF8)
     version, status, explanation = statusline.split(" ", 2)
     assert status == "200", "{}: {}".format(status, explanation)
 
     headers = {}
     while True:
-        line = response.readline().decode(ENCODING)
-        if line == CRLF:
+        line = response.readline().decode(ENCODING_UTF8)
+        if line == NEWLINE:
             break
         header, value = line.split(":", 1)
         headers[header.lower()] = value.strip()
 
     encoding = headers.get(HEADER_CONTENT_ENCODING, "")
-    if encoding.lower() == GZIP_ENCODING:
-        body = gzip.decompress(response.read()).decode(ENCODING)
+    if encoding.lower() == ENCODING_GZIP:
+        body = gzip.decompress(response.read()).decode(ENCODING_UTF8)
     else:
-        body = response.read().decode(ENCODING)
+        body = response.read().decode(ENCODING_UTF8)
 
     s.close()
 
